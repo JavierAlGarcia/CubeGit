3bea1dbb040a64acb4c7c703b3106d52c3f6953b
JavierAlvarezGarcia <124053595+JavierAlGarcia@users.noreply.github.com>
2025-01-11 02:41:34 +0100

    Inicio de implemencation comando cat-file

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -17,19 +17,29 @@ func main() {
 	case "init":
 		for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
 			if err := os.MkdirAll(dir, 0755); err != nil {
-				fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+				fmt.Fprintf(os.Stderr, "Error creando directorio: %s\n", err)
 			}
 		}
 
 		headFileContents := []byte("ref: refs/heads/main\n")
 		if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+			fmt.Fprintf(os.Stderr, "Error escribiendo archivo: %s\n", err)
 		}
 
-		fmt.Println("Initialized git directory")
+		fmt.Println("Directorio git inicializado")
+
+	case "cat-file":
+		hash_sha1 := os.Args[3]
+		ruta_fichero := fmt.Sprintf(".git/objects/%s/%s", hash_sha1[0:2], hash_sha1[2:])
+		fichero, error := os.Open(ruta_fichero)
+		if error != nil {
+			fmt.Fprintf(os.Stderr, "Error abriendo archivo: %s\n", error)
+			os.Exit(1)
+		}
+		defer fichero.Close()
 
 	default:
-		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
+		fmt.Fprintf(os.Stderr, "Comando desconocido %s\n", command)
 		os.Exit(1)
 	}
 }
